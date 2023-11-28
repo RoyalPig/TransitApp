400f239cd5abea301579658d373f78d69e467869
Matthew MacKay <W0437676@campus.nscc.ca>
2023-11-28 19:02:26 -0400

    Route annotations look good

--- app/src/main/java/com/example/transitapp/ui/Map/MapFragment.kt
+++ app/src/main/java/com/example/transitapp/ui/Map/MapFragment.kt
@@ -1,27 +1,24 @@
 package com.example.transitapp.ui.Map
 
-import android.graphics.BitmapFactory
+import android.graphics.Bitmap
+import android.graphics.Canvas
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
 import com.example.transitapp.R
 import com.example.transitapp.databinding.FragmentMapBinding
-import com.example.transitapp.ui.Map.MapViewModel
 import com.google.transit.realtime.GtfsRealtime
 import com.mapbox.geojson.Point
 import com.mapbox.maps.MapboxMap
 import com.mapbox.maps.Style
-import com.mapbox.maps.plugin.annotation.generated.PointAnnotationOptions
-import java.net.URL
 import com.mapbox.maps.plugin.annotation.annotations
-import com.mapbox.maps.plugin.Plugin
 import com.mapbox.maps.plugin.annotation.generated.PointAnnotationManager
+import com.mapbox.maps.plugin.annotation.generated.PointAnnotationOptions
 import com.mapbox.maps.plugin.annotation.generated.createPointAnnotationManager
-
+import java.net.URL
 
 class MapFragment : Fragment() {
 
@@ -29,8 +26,6 @@ class MapFragment : Fragment() {
     private val binding get() = _binding!!
     private lateinit var mapboxMap: MapboxMap
     private lateinit var pointAnnotationManager: PointAnnotationManager
-
-
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -60,10 +55,22 @@ class MapFragment : Fragment() {
                             val latitude = vehicle.position.latitude
                             val longitude = vehicle.position.longitude
 
-                            // Create a point annotation for each bus position
+                            // Custom annotation
+                            val customAnnotationView = LayoutInflater.from(context).inflate(R.layout.bus_annotation_layout, null)
+                            val textView = customAnnotationView.findViewById<TextView>(R.id.busNumberText)
+                            textView.text = routeId // Set the bus number
+
+                            // Convert the view to Bitmap
+                            customAnnotationView.measure(View.MeasureSpec.UNSPECIFIED, View.MeasureSpec.UNSPECIFIED)
+                            customAnnotationView.layout(0, 0, customAnnotationView.measuredWidth, customAnnotationView.measuredHeight)
+                            val bitmap = Bitmap.createBitmap(customAnnotationView.measuredWidth, customAnnotationView.measuredHeight, Bitmap.Config.ARGB_8888)
+                            val canvas = Canvas(bitmap)
+                            customAnnotationView.draw(canvas)
+
+                            // Create a point annotation for each bus position with custom view
                             val pointAnnotation = PointAnnotationOptions()
                                 .withPoint(Point.fromLngLat(longitude.toDouble(), latitude.toDouble()))
-                                .withTextField(routeId) // This sets the text field
+                                .withIconImage(bitmap)
 
                             annotations.add(pointAnnotation)
                         }
